3a9073584d054d282d972ddaf41c1f4024680698
alexsharich <alexsharich@gmail.com>
2021-02-25 22:08:14 +0300

    feat : add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr) {
+  if (!arr || arr.length === 0) return 0
+  return Math.min.apply(null, arr);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+  if (!arr || arr.length === 0) return 0
+
+  return Math.max.apply(null, arr);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+  if (!arr || arr.length === 0) return 0
+  let sum = 0
+  for (let elem of arr) {
+    sum += elem
+  }
+  return sum / arr.length
 }
